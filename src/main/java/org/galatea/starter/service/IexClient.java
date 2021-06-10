cb54d01ec397806bd88a88fb9fef1540862d32ff
Aaron Portman <aportman@Galatea.com>
2021-06-10 09:54:28 -0400

    FixIexTest

--- src/main/java/org/galatea/starter/service/IexClient.java
+++ src/main/java/org/galatea/starter/service/IexClient.java
@@ -1,11 +1,13 @@
 package org.galatea.starter.service;
 
 import java.util.List;
-import org.galatea.starter.domain.IexLastTradedPrice;
+
+import org.galatea.starter.domain.IexHistoricalPrice;
 import org.galatea.starter.domain.IexSymbol;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.galatea.starter.domain.IexLastTradedPrice;
 
 /**
  * A Feign Declarative REST Client to access endpoints from the Free and Open IEX API to get market
@@ -31,5 +33,7 @@ public interface IexClient {
    */
   @GetMapping("/tops/last")
   List<IexLastTradedPrice> getLastTradedPriceForSymbols(@RequestParam("symbols") String[] symbols);
+
+
 
 }
